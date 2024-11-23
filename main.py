7bdbcfa57a82109248b355f8da4decf986742b30
Adil Muratov <muratowadil@gmail.com>
2024-11-23 22:28:36 +0300

    ninth update

--- main.py
+++ main.py
@@ -19,150 +19,174 @@ def gameplay():
     for i in range(len(place)):
         place[i] = ["[0]"] * 7
         visible_place[i] = ["[*]"] * 7
+    #generation ships
+    while True:
+        fresher = 0
+        counter = 0
 
-    #generation one point ships
-    while count_one_point_ship != 0:
-        checker = 1
-        ship_row = randint(0, 6)
-        ship_column = randint(0, 6)
+        #generation one point ships
+        while count_one_point_ship != 0:
+            checker = 1
+            ship_row = randint(0, 6)
+            ship_column = randint(0, 6)
 
-        for f in range(len(taken_place)):
-            if taken_place[f][0] == ship_row and taken_place[f][1] == ship_column:
-                checker = 0
-
-        if checker == 1:
-            place[ship_row][ship_column] = "[1]"
-            taken_place.append([ship_row, ship_column])
-            taken_place.append([ship_row - 1, ship_column])
-            taken_place.append([ship_row - 1, ship_column - 1])
-            taken_place.append([ship_row - 1, ship_column + 1])
-            taken_place.append([ship_row, ship_column - 1])
-            taken_place.append([ship_row, ship_column + 1])
-            taken_place.append([ship_row + 1, ship_column - 1])
-            taken_place.append([ship_row + 1, ship_column])
-            taken_place.append([ship_row + 1, ship_column + 1])
-            count_one_point_ship -= 1
-
-    #generation two points ships
-    while count_two_point_ship != 0:
-        checker_second_point = 1
-        checker = 1
-        ship_row1 = randint(0, 6)
-        ship_column1 = randint(0, 6)
-        useful_points = [
-            (ship_row1 + 1, ship_column1),
-            (ship_row1, ship_column1 + 1),
-            (ship_row1 - 1, ship_column1),
-            (ship_row1, ship_column1 - 1)
-        ]
-        ship_row2 = randint(0, 6)
-        ship_column2 = randint(0, 6)
-
-        while checker_second_point != 0:
-            if (ship_row2, ship_column2) in useful_points:
-                checker_second_point = 0
-            else:
-                ship_row2 = randint(0, 6)
-                ship_column2 = randint(0, 6)
-
-        for f in range(len(taken_place)):
-            if taken_place[f][0] == ship_row1 and taken_place[f][1] == ship_column1:
-                checker = 0
-            else:
-                if taken_place[f][0] == ship_row2 and taken_place[f][1] == ship_column2:
+            for f in range(len(taken_place)):
+                if taken_place[f][0] == ship_row and taken_place[f][1] == ship_column:
                     checker = 0
-        if checker == 1:
-            place[ship_row1][ship_column1] = "[2]"
-            taken_place.append([ship_row1, ship_column1])
-            taken_place.append([ship_row1 - 1, ship_column1])
-            taken_place.append([ship_row1 - 1, ship_column1 - 1])
-            taken_place.append([ship_row1 - 1, ship_column1 + 1])
-            taken_place.append([ship_row1, ship_column1 - 1])
-            taken_place.append([ship_row1, ship_column1 + 1])
-            taken_place.append([ship_row1 + 1, ship_column1 - 1])
-            taken_place.append([ship_row1 + 1, ship_column1])
-            taken_place.append([ship_row1 + 1, ship_column1 + 1])
 
-            place[ship_row2][ship_column2] = "[2]"
-            taken_place.append([ship_row2, ship_column2])
-            taken_place.append([ship_row2 - 1, ship_column2])
-            taken_place.append([ship_row2 - 1, ship_column2 - 1])
-            taken_place.append([ship_row2 - 1, ship_column2 + 1])
-            taken_place.append([ship_row2, ship_column2 - 1])
-            taken_place.append([ship_row2, ship_column2 + 1])
-            taken_place.append([ship_row2 + 1, ship_column2 - 1])
-            taken_place.append([ship_row2 + 1, ship_column2])
-            taken_place.append([ship_row2 + 1, ship_column2 + 1])
+            if checker == 1:
+                place[ship_row][ship_column] = "[1]"
+                taken_place.append([ship_row, ship_column])
+                taken_place.append([ship_row - 1, ship_column])
+                taken_place.append([ship_row - 1, ship_column - 1])
+                taken_place.append([ship_row - 1, ship_column + 1])
+                taken_place.append([ship_row, ship_column - 1])
+                taken_place.append([ship_row, ship_column + 1])
+                taken_place.append([ship_row + 1, ship_column - 1])
+                taken_place.append([ship_row + 1, ship_column])
+                taken_place.append([ship_row + 1, ship_column + 1])
+                count_one_point_ship -= 1
 
-            count_two_point_ship -= 1
+        #generation two points ships
+        while count_two_point_ship != 0:
+            checker_second_point = 1
+            checker = 1
+            ship_row1 = randint(0, 6)
+            ship_column1 = randint(0, 6)
+            useful_points = [
+                (ship_row1 + 1, ship_column1),
+                (ship_row1, ship_column1 + 1),
+                (ship_row1 - 1, ship_column1),
+                (ship_row1, ship_column1 - 1)
+            ]
+            ship_row2 = randint(0, 6)
+            ship_column2 = randint(0, 6)
 
-    #generation three points ship
-    while count_three_point_ship != 0:
-        checker_second_point = 1
-        checker = 1
-        ship_row1 = randint(0, 6)
-        ship_column1 = randint(0, 6)
-        useful_points1 = [
-            (ship_row1 + 1, ship_column1),
-            (ship_row1, ship_column1 + 1),
-            (ship_row1 - 1, ship_column1),
-            (ship_row1, ship_column1 - 1)
-        ]
-        ship_row2 = randint(0, 6)
-        ship_column2 = randint(0, 6)
+            while checker_second_point != 0:
+                if (ship_row2, ship_column2) in useful_points:
+                    checker_second_point = 0
+                else:
+                    ship_row2 = randint(0, 6)
+                    ship_column2 = randint(0, 6)
 
-        while checker_second_point != 0:
-            if (ship_row2, ship_column2) in useful_points1:
-                checker_second_point = 0
-            else:
-                ship_row2 = randint(0, 6)
-                ship_column2 = randint(0, 6)
-
-        useful_points2 = []
-
-        if ship_row1 == ship_row2:
-            if ship_column1 - ship_column2 < 0:
-                useful_points2.append((ship_row1, ship_column1 - 1))
-                useful_points2.append((ship_row2, ship_column2 + 1))
-            else:
-                useful_points2.append((ship_row2, ship_column2 - 1))
-                useful_points2.append((ship_row1, ship_column1 + 1))
-        elif ship_column1 == ship_column2:
-            if ship_row1 - ship_row2 < 0:
-                useful_points2.append((ship_row1 - 1, ship_column1))
-                useful_points2.append((ship_row2 + 1, ship_column2))
-            else:
-                useful_points2.append((ship_row2 - 1, ship_column2))
-                useful_points2.append((ship_row1 + 1, ship_column1))
-
-        if useful_points2[0] not in taken_place and useful_points2[0][0] > 0 and useful_points2[0][1] > 0:
-            ship_row3 = useful_points2[0][0]
-            ship_column3 = useful_points2[0][1]
-        elif useful_points2[1] not in taken_place and useful_points2[1][0] > 0 and useful_points2[1][1] > 0:
-            ship_row3 = useful_points2[1][0]
-            ship_column3 = useful_points2[1][1]
-        else:
-            continue
-
-        for f in range(len(taken_place)):
-            if taken_place[f][0] == ship_row1 and taken_place[f][1] == ship_column1:
-                checker = 0
-            else:
-                if taken_place[f][0] == ship_row2 and taken_place[f][1] == ship_column2:
+            for f in range(len(taken_place)):
+                if taken_place[f][0] == ship_row1 and taken_place[f][1] == ship_column1:
                     checker = 0
                 else:
-                    if taken_place[f][0] == ship_row3 and taken_place[f][1] == ship_column3:
+                    if taken_place[f][0] == ship_row2 and taken_place[f][1] == ship_column2:
                         checker = 0
+            if checker == 1:
+                place[ship_row1][ship_column1] = "[2]"
+                taken_place.append([ship_row1, ship_column1])
+                taken_place.append([ship_row1 - 1, ship_column1])
+                taken_place.append([ship_row1 - 1, ship_column1 - 1])
+                taken_place.append([ship_row1 - 1, ship_column1 + 1])
+                taken_place.append([ship_row1, ship_column1 - 1])
+                taken_place.append([ship_row1, ship_column1 + 1])
+                taken_place.append([ship_row1 + 1, ship_column1 - 1])
+                taken_place.append([ship_row1 + 1, ship_column1])
+                taken_place.append([ship_row1 + 1, ship_column1 + 1])
 
-        if checker == 1:
-            place[ship_row1][ship_column1] = "[3]"
-            place[ship_row2][ship_column2] = "[3]"
-            place[ship_row3][ship_column3] = "[3]"
+                place[ship_row2][ship_column2] = "[2]"
+                taken_place.append([ship_row2, ship_column2])
+                taken_place.append([ship_row2 - 1, ship_column2])
+                taken_place.append([ship_row2 - 1, ship_column2 - 1])
+                taken_place.append([ship_row2 - 1, ship_column2 + 1])
+                taken_place.append([ship_row2, ship_column2 - 1])
+                taken_place.append([ship_row2, ship_column2 + 1])
+                taken_place.append([ship_row2 + 1, ship_column2 - 1])
+                taken_place.append([ship_row2 + 1, ship_column2])
+                taken_place.append([ship_row2 + 1, ship_column2 + 1])
 
-            count_three_point_ship -= 1
+                count_two_point_ship -= 1
 
+        #generation three points ship
+        while count_three_point_ship != 0:
+            counter += 1
+            if counter > 10000:
+                fresher += 1
+                break
+
+            checker_second_point = 1
+            checker = 1
+            ship_row1 = randint(0, 6)
+            ship_column1 = randint(0, 6)
+            useful_points1 = [
+                (ship_row1 + 1, ship_column1),
+                (ship_row1, ship_column1 + 1),
+                (ship_row1 - 1, ship_column1),
+                (ship_row1, ship_column1 - 1)
+            ]
+            ship_row2 = randint(0, 6)
+            ship_column2 = randint(0, 6)
+
+            while checker_second_point != 0:
+                if (ship_row2, ship_column2) in useful_points1:
+                    checker_second_point = 0
+                else:
+                    ship_row2 = randint(0, 6)
+                    ship_column2 = randint(0, 6)
+
+            useful_points2 = []
+
+            if ship_row1 == ship_row2:
+                if ship_column1 - ship_column2 < 0:
+                    useful_points2.append((ship_row1, ship_column1 - 1))
+                    useful_points2.append((ship_row2, ship_column2 + 1))
+                else:
+                    useful_points2.append((ship_row2, ship_column2 - 1))
+                    useful_points2.append((ship_row1, ship_column1 + 1))
+            elif ship_column1 == ship_column2:
+                if ship_row1 - ship_row2 < 0:
+                    useful_points2.append((ship_row1 - 1, ship_column1))
+                    useful_points2.append((ship_row2 + 1, ship_column2))
+                else:
+                    useful_points2.append((ship_row2 - 1, ship_column2))
+                    useful_points2.append((ship_row1 + 1, ship_column1))
+
+            if useful_points2[0] not in taken_place and useful_points2[0][0] > 0 and useful_points2[0][1] > 0:
+                ship_row3 = useful_points2[0][0]
+                ship_column3 = useful_points2[0][1]
+            elif useful_points2[1] not in taken_place and useful_points2[1][0] > 0 and useful_points2[1][1] > 0:
+                ship_row3 = useful_points2[1][0]
+                ship_column3 = useful_points2[1][1]
+            else:
+                continue
+
+            for f in range(len(taken_place)):
+                if taken_place[f][0] == ship_row1 and taken_place[f][1] == ship_column1:
+                    checker = 0
+                else:
+                    if taken_place[f][0] == ship_row2 and taken_place[f][1] == ship_column2:
+                        checker = 0
+                    else:
+                        if taken_place[f][0] == ship_row3 and taken_place[f][1] == ship_column3:
+                            checker = 0
+
+            if checker == 1:
+                place[ship_row1][ship_column1] = "[3]"
+                place[ship_row2][ship_column2] = "[3]"
+                place[ship_row3][ship_column3] = "[3]"
+
+                count_three_point_ship -= 1
+
+        if fresher == 1:
+            #print("bug")
+            count_one_point_ship = 4
+            count_two_point_ship = 2
+            taken_place = []
+            for i in range(len(place)):
+                place[i] = ["[0]"] * 7
+            continue
+
+        break
+
+    count_killed_ships = 0
     used_points = []
     coordinates_three_points_ship = []
+
+    for i in range(7):
+        print(place[i])
 
     #game process
     while True:
@@ -178,7 +202,8 @@ def gameplay():
             os.system("cls")
             print("Please enter coordinates right")
             continue
-        
+
+        coordinates = coordinates.split()
         a = int(coordinates[0]) - 1
         b = int(coordinates[1]) - 1
 
@@ -191,21 +216,44 @@ def gameplay():
             visible_place[a][b] = "[m]"
         elif place[a][b] == "[1]":
             visible_place[a][b] = "[d]"
+            count_killed_ships += 1
         elif place[a][b] == "[2]":
-            if visible_place[a - 1][b] == "[h]":
-                visible_place[a][b] = "[d]"
-                visible_place[a - 1][b] = "[d]"
-            elif visible_place[a + 1][b] == "[h]":
-                visible_place[a][b] = "[d]"
-                visible_place[a + 1][b] = "[d]"
-            elif visible_place[a][b - 1] == "[h]":
-                visible_place[a][b] = "[d]"
-                visible_place[a][b - 1] = "[d]"
-            elif visible_place[a][b + 1] == "[h]":
-                visible_place[a][b] = "[d]"
-                visible_place[a][b + 1] = "[d]"
-            else:
-                visible_place[a][b] = "[h]"
+            try:
+                if visible_place[a - 1][b] == "[h]":
+                    visible_place[a][b] = "[d]"
+                    visible_place[a - 1][b] = "[d]"
+                    count_killed_ships += 1
+                elif visible_place[a - 1][b] == "[*]" and visible_place[a][b] == "[*]":
+                    visible_place[a][b] = "[h]"
+            except IndexError:
+                pass
+            try:
+                if visible_place[a + 1][b] == "[h]":
+                    visible_place[a][b] = "[d]"
+                    visible_place[a + 1][b] = "[d]"
+                    count_killed_ships += 1
+                elif visible_place[a + 1][b] == "[*]" and visible_place[a][b] == "[*]":
+                    visible_place[a][b] = "[h]"
+            except IndexError:
+                pass
+            try:
+                if visible_place[a][b + 1] == "[h]":
+                    visible_place[a][b] = "[d]"
+                    visible_place[a][b + 1] = "[d]"
+                    count_killed_ships += 1
+                elif visible_place[a][b + 1] == "[*]" and visible_place[a][b] == "[*]":
+                    visible_place[a][b] = "[h]"
+            except IndexError:
+                pass
+            try:
+                if visible_place[a][b - 1] == "[h]":
+                    visible_place[a][b] = "[d]"
+                    visible_place[a][b - 1] = "[d]"
+                    count_killed_ships += 1
+                elif visible_place[a][b - 1] == "[*]" and visible_place[a][b] == "[*]":
+                    visible_place[a][b] = "[h]"
+            except IndexError:
+                pass
         elif place[a][b] == "[3]":
             if len(coordinates_three_points_ship) != 2:
                 visible_place[a][b] = "[h]"
@@ -214,13 +262,14 @@ def gameplay():
                 visible_place[a][b] = "[d]"
                 visible_place[coordinates_three_points_ship[0][0]][coordinates_three_points_ship[0][1]] = "[d]"
                 visible_place[coordinates_three_points_ship[1][0]][coordinates_three_points_ship[1][1]] = "[d]"
+                count_killed_ships += 1
 
 
         score += 1
         used_points.append((a, b))
         os.system("cls")
 
-        if score >= limiter:
+        if score >= limiter or count_killed_ships == 7:
             break
 
     return score
--- test.py
+++ test.py
@@ -1,9 +1,3 @@
-place = [0] * 5
+from threading import Timer
 
-for i in range(len(place)):
-    place[i] = [0] * 5
 
-place[-1][-1] = "x"
-
-for i in place:
-    print(i)
